337e18ffa92b55a6ca0b28da4447cc7cfb6212c6
Tjaart van der Walt <git@tjaart.org>
2021-04-27 23:13:46 -0700

    Handle newly added tests (#6)
    fixes #4

--- dist/index.js
+++ dist/index.js
@@ -2806,6 +2806,28 @@ async function weights(files) {
     core.info(`Weights file found at ${filename}`)
     weightsFile = fs.readFileSync(filename, "utf8")
     fileWeights = JSON.parse(weightsFile)
+
+    let fileWeightsKeys = Object.keys(fileWeights)
+    let difference = files.filter((x) => !fileWeightsKeys.includes(x))
+    let returnObject = {}
+    difference.forEach((el) => {
+      fileWeights[el] = 1
+    })
+    if (difference.length > 0) {
+      core.warning(
+        `The following files do not have weights assigned: ${difference}`
+      )
+    }
+
+    difference = fileWeightsKeys.filter((x) => !files.includes(x))
+    difference.forEach((el) => {
+      delete fileWeights[el]
+    })
+    if (difference.length > 0) {
+      core.warning(
+        `The following files are not matched by the spec glob, but have weights assigned: ${difference}`
+      )
+    }
   } else {
     core.info(`Weights file not found at ${filename}. Using default weights.`)
     let returnObject = {}
