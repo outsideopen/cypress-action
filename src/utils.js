8277b44c42df05b9ba537137e29b5d272e7b546e
Tjaart van der Walt <git@tjaart.org>
2021-08-22 17:30:17 -0700

    Calculate the average weight at each iteration
    If you don't recalculate the average for each iteration, you risk not
    executing some of the last iterations

--- src/utils.js
+++ src/utils.js
@@ -2,18 +2,18 @@ const knapsack = require("knapsack-js")
 
 module.exports = { executionPlan }
 
+function remainingWeight(weightedArray) {
+  let weights = weightedArray.map((x) => Object.values(x)[0])
+  let weight = weights.reduce((accumulator, el) => accumulator + el)
+  return weight
+}
+
 function executionPlan(weightedFiles, nrOfGroups) {
   if (!weightedFiles || Object.keys(weightedFiles).length == 0) {
     return []
   }
 
   let executionPlan = []
-
-  let weights = Object.values(weightedFiles)
-
-  let totalWeight = weights.reduce((accumulator, el) => accumulator + el)
-
-  const weightPerGroup = Math.ceil(totalWeight / nrOfGroups)
 
   weightedArray = []
   for (const key in weightedFiles) {
@@ -23,9 +23,13 @@ function executionPlan(weightedFiles, nrOfGroups) {
   }
 
   for (let i = 0; i < nrOfGroups; i++) {
-    let nextGroup = knapsack.resolve(weightPerGroup, weightedArray)
+    const weight = remainingWeight(weightedArray)
+    const weightPerGroup = Math.ceil(weight / (nrOfGroups - i))
+
+    const nextGroup = knapsack.resolve(weightPerGroup, weightedArray)
     const keys = nextGroup.map((el) => Object.keys(el)[0])
     executionPlan.push(keys)
+
     keys.forEach((key) => {
       weightedArray = weightedArray.filter(
         (filter) => Object.keys(filter)[0] != key
